e2c6009f507ba60065fccf60f95b924d85a3e0d4
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-29 19:43:29 +0700

    da

--- NoOverriding/NoOverriding.cpp
+++ NoOverriding/NoOverriding.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan()final {
+	virtual void perkenalan() final {
 		cout << "Haiiiiiiii saya Function dari base class";
 	}
 };
 
 class derivedClass : public baseClass {
-
+public:
+	void perkenalan() {
+		cout << "Haiiiiiiii saya Function dari derived class";
+	}
 };
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
