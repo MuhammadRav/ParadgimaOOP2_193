62fd38b3215e81955947005a6b0efa014be2fa93
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-29 15:25:12 +0700

    dd

--- Problem Multi Warisan/Problem Multi Warisan.cpp
+++ Problem Multi Warisan/Problem Multi Warisan.cpp
@@ -20,3 +20,22 @@ public:
 		cout << "Pekerja dibuat\n" << endl;
 	}
 };
+
+class pelajar : public orang {
+public:
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pelajar dibuat\n" << endl;
+	}
+};
+
+class budi :public pekerja, public pelajar {
+public:
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
